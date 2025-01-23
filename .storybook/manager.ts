07b68354e672fd46c5478148d84f9e2a564634c9
Merieli Manzano <merieli.amanzano@gmail.com>
2025-01-23 16:03:00 -0300

    chore: add badges and docs about components lifecycle

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,7 @@
 import { addons } from '@storybook/manager-api';
 import { API_PreparedIndexEntry, API_StatusObject } from '@storybook/types';
 import theme from './themes/ds';
+import { defaultConfig, type TagBadgeParameters, renderLabel } from 'storybook-addon-tag-badges';
 
 addons.setConfig({
   theme,
@@ -14,5 +15,48 @@ addons.setConfig({
         return !(item.tags ?? []).includes('hideInSidebar');
       },
     },
+    renderLabel,
   },
+  tagBadges: [
+    {
+      tags: 'experimental',
+      badge: {
+        text: 'Experimental',
+        bgColor: '#FFF1D6',
+        fgColor: '#6F4A0B',
+        tooltip: 'Componente em fase experimental, sujeito a quebra e mudanças.',
+      },
+      display: {
+        sidebar: ['component'],
+        toolbar: true,
+      },
+    },
+    {
+      tags: 'beta',
+      badge: {
+        text: 'Beta',
+        bgColor: '#E7E3FC',
+        fgColor: '#3C309C',
+        tooltip: 'Versão próxima de `stable` mas ainda sujeito a ajustes.',
+      },
+      display: {
+        sidebar: ['component'],
+        toolbar: true,
+      },
+    },
+    {
+      tags: 'deprecated',
+      badge: {
+        text: 'Deprecated',
+        bgColor: '#FDE2E5',
+        fgColor: '#8B2331',
+        tooltip: 'Este componente foi marcado como obsoleto e será removido em breve. Não use!',
+      },
+      display: {
+        sidebar: ['component'],
+        toolbar: true,
+      },
+    },
+    ...defaultConfig,
+  ] satisfies TagBadgeParameters,
 });
